60727eb6315d4e8e1c6c0dc7f318a26f7fa8e4de
FauxKiwi <sfn14ok04@gmail.com>
2020-11-09 20:50:19 +0100

    Started making controls

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerControls : MonoBehaviour
 {
-    public float SPEED = 1;
+    [FormerlySerializedAs("SPEED")] public float speed = 10f;
     private Transform _transform;
     
     // Start is called before the first frame update
@@ -16,9 +17,7 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            _transform.Translate(Time.deltaTime * SPEED, 0, 0);
-        }
+        _transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 
+            Input.GetAxis("Vertical") * Time.deltaTime * speed, 0);
     }
 }
