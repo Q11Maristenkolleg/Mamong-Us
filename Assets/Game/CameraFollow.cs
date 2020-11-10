afc73e8d22ce3de2918ff2a9c62659cfc48530a3
FauxKiwi <sfn14ok04@gmail.com>
2020-11-10 19:37:12 +0100

    Updated structure

---
+++ Assets/Game/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public PlayerControls player;
+    private Transform _transform;
+    private Vector3 _distance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = player.GetPosition() - _transform.position;
+        _distance.z = 0;
+        _transform.Translate(_distance);
+    }
+}
