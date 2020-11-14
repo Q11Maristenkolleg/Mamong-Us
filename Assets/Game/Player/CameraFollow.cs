5a4a0ea035bf0b422031703204a8e89a1c2d1db5
FauxKiwi <sfn14ok04@gmail.com>
2020-11-14 18:58:33 +0100

    Added multiplayer

---
+++ Assets/Game/Player/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Transform cameraPosition;
+
+    void FixedUpdate()
+    {
+        var distance = playerPosition.position - cameraPosition.position;
+        distance.z = 0;
+        cameraPosition.Translate(distance * (Time.fixedDeltaTime * 10));
+    }
+}
