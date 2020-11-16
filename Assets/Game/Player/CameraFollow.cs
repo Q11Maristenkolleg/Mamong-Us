8dddbad24f63711ad13bcf0ca379af4c58d94505
FauxKiwi <sfn14ok04@gmail.com>
2020-11-16 19:33:18 +0100

    Multiplayer working now

--- Assets/Game/Player/CameraFollow.cs
+++ Assets/Game/Player/CameraFollow.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollow : NetworkBehaviour
 {
     public Transform playerPosition;
     public Transform cameraPosition;
 
     void FixedUpdate()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
         var distance = playerPosition.position - cameraPosition.position;
         distance.z = 0;
         cameraPosition.Translate(distance * (Time.fixedDeltaTime * 10));
---
+++ Assets/Game/Player/NetworkEnable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Networking;
+
+public class NetworkEnable : NetworkBehaviour
+{
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        GetComponent<PlayerControls>().enabled = true;
+        GetComponent<CameraFollow>().enabled = true;
+        if (Camera.main != null)
+        {
+            GetComponent<CameraFollow>().cameraPosition = Camera.main.GetComponent<Transform>();
+        }
+        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<Light2D>().enabled = true;
+    }
+}
